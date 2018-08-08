5b459785da6cfa9e4019d73c185a0b9237c85fea
Rane M Kaseman <raykman@comcast.net>
2018-08-08 01:20:14 -0600

    0120

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Trivia-Game
+
+Shoegazer trivia
+
+create and start timer
+
+bands:
+ 1. throwing muses
+ 2. medicine
+ 3. jesus and mary chain
+ 4. lush
+ 5. cocteau twins
+ 6. mazzy star
+ 7. belly
+ 8. pale saints
+ 9. the sundays
+10. my bloody valentine
+
+images for all
+
---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+
+var audio1 = new Audio("../assets/sounds/throwing muses doghouse 2 14.mp3");
+
+//  Step 2:
+//  after 5 seconds, execute the fiveSeconds function
+//  after 10 seconds, execute the tenSeconds function
+//  after 15 seconds, execute the timeUp function
+
+setTimeout(fiveSeconds, 1000 * 5);
+setTimeout(tenSeconds, 1000 * 10);
+setTimeout(timeUp, 1000 * 15);
+
+//  Step 3:
+//  Fill in the blanks to these functions.
+function fiveSeconds() {
+
+  // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
+  // console log 10 seconds left
+  $("#time-left").append("<h2>About 10 Seconds Left!</h2>");
+  console.log("10 seconds left");
+}
+
+function tenSeconds() {
+
+  // in the element with an id of time-left add an h2 saying About 5 Seconds Left!
+  // console log 5 seconds left
+  $("#time-left").append("<h2>About 5 Seconds Left!</h2>");
+  console.log("5 seconds left");
+}
+
+function timeUp() {
+
+  // in the element with an id of time-left add an h2 saying Time's Up!
+  // console log done
+  console.log("done");
+  $("#time-left").append("<h2>Time's Up!</h2>");
+  console.log("time is up");
+
+  //  The following line will play the audio1 file we linked to above:
+  audio1.play();
+}
+
